6eadb3e37b4e6745aeefffb8ece738b6909533ad
Theophilus <tkayetor@users.noreply.github.com>
2017-04-03 06:43:42 -0700

    _s WordPress Template called mimi
    This is a completely blank developer template. I got it from underscore s, which is a blank template theme file for WordPress Developers.

---
+++ mimi/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Mimi\'s_Style
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mimi' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mimi' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mimi' ), 'mimi', '<a href="https://automattic.com/" rel="designer">Theophilus Tor</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
